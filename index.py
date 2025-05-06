880a9cc155b796cf276b00d334af127861a38bf3
Uomoninja189 <155568173+Uomoninja189@users.noreply.github.com>
2025-05-06 09:58:26 +0200

    Update index.py

--- index.py
+++ index.py
@@ -5,5 +5,7 @@ import callbacks.callback_crea
 import callbacks.callback_impostazioni
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False,
+        host='0.0.0.0',
+        port=int(os.environ.get("PORT", 8050)))
     
